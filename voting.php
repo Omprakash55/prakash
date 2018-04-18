ef7c7a2fad5faf84e72e2a7ccaf80d6b619c7aae
Omprakash55 <38484784+Omprakash55@users.noreply.github.com>
2018-04-18 09:56:58 +0530

    Create voting.php

---
+++ voting.php
@@ -0,0 +1,13 @@
+<?php
+	echo'<form id="myform">';
+	echo'<input type="radio" name="candidate" id="name1" value="Rahul"/>om<br />';
+	 echo '<form id="myform">';
+	echo'<input type="radio" name="candidate" id="name2" value="Suhas" />prakash<br />';
+ echo '<form id="myform">';
+	echo'<input type="radio" name="candidate" id="name3" value="Shreyas"/>sunny<br />';
+
+	echo '</form>';
+?>
+
+
+
